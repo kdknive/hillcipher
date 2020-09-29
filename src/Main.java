3c37323601714796c2add994cefe500ad0c79d95
kdknive <kevindknive@gmail.com>
2020-09-29 21:37:26 +0700

    aaarrgghh

---
+++ src/Main.java
@@ -0,0 +1,410 @@
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    int[] lm;
+    int[] lm2;
+    int[][] km;
+    int[] rm;
+    static int choice;
+    int [][] invK;
+
+    public void performDivision(String temp, int s)
+    {
+        while (temp.length() > s)
+        {
+            String line = temp.substring(0, s);
+            temp = temp.substring(s, temp.length());
+            calLineMatrix(line);
+            if(choice ==1){
+                multiplyLineByKey(line.length());
+            }else if(choice==2){
+                multiplyLineByInvKey(line.length());
+            }
+            showResult(line.length());
+        }
+        if (temp.length() == s){
+
+            if(choice ==1){
+                calLineMatrix(temp);
+                multiplyLineByKey(temp.length());
+                showResult(temp.length());
+            }
+            else if(choice==2){
+                calLineMatrix(temp);
+                this.multiplyLineByInvKey(temp.length());
+                showResult(temp.length());
+            }
+        }
+        else if (temp.length() < s)
+        {
+            for (int i = temp.length(); i < s; i++)
+                temp = temp + 'x';
+            if(choice ==1){
+                calLineMatrix(temp);
+                multiplyLineByKey(temp.length());
+                showResult(temp.length());
+            }
+            else if(choice==2){
+                calLineMatrix(temp);
+                multiplyLineByInvKey(temp.length());
+                showResult(temp.length());
+            }
+        }
+    }
+
+
+    public void calKeyMatrix(String key, int len)
+    {
+        km = new int[len][len];
+        int k = 0;
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                km[i][j] = ((int) key.charAt(k)) - 97;
+                k++;
+            }
+        }
+    }
+
+    public void calLineMatrix(String line)
+    {
+        lm = new int[line.length()];
+        for (int i = 0; i < line.length(); i++)
+        {
+            lm[i] = ((int) line.charAt(i)) - 97;
+        }
+    }
+
+    public void calLineMatrix2(String key)
+    {
+        lm2 = new int[key.length()];
+        for (int i = 0; i < key.length(); i++)
+        {
+            lm2[i] = ((int) key.charAt(i)) - 97;
+        }
+    }
+
+    public void multiplyLineByKey(int len)
+    {
+        rm = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                rm[i] += km[i][j] * lm[j];
+            }
+            rm[i] %= 26;
+        }
+    }
+    public void multiplyLineByInvKey(int len)
+    {
+
+        rm = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                rm[i] += invK[i][j] * lm[j];
+            }
+            rm[i] %= 26;
+        }
+    }
+
+    public void showResult(int len)
+    {
+        String result = "";
+        for (int i = 0; i < len; i++)
+        {
+            result += (char) (rm[i] + 97);
+        }
+        System.out.print(result);
+    }
+
+
+
+    public int calDeterminant(int A[][], int N)
+    {
+        int resultOfDet;
+        switch (N) {
+            case 1:
+                resultOfDet = A[0][0];
+                break;
+            case 2:
+                resultOfDet = A[0][0] * A[1][1] - A[1][0] * A[0][1];
+                break;
+            default:
+                resultOfDet = 0;
+                for (int j1 = 0; j1 < N; j1++)
+                {
+                    int m[][] = new int[N - 1][N - 1];
+                    for (int i = 1; i < N; i++)
+                    {
+                        int j2 = 0;
+                        for (int j = 0; j < N; j++)
+                        {
+                            if (j == j1)
+                                continue;
+                            m[i - 1][j2] = A[i][j];
+                            j2++;
+                        }
+                    }
+                    resultOfDet += Math.pow(-1.0, 1.0 + j1 + 1.0) * A[0][j1]
+                            * calDeterminant(m, N - 1);
+                }   break;
+        }
+        return resultOfDet;
+    }
+
+    public void cofact(int num[][], int f)
+    {
+        int b[][], fac[][];
+        b = new int[f][f];
+        fac = new int[f][f];
+        int p, q, m, n, i, j;
+        for (q = 0; q < f; q++)
+        {
+            for (p = 0; p < f; p++)
+            {
+                m = 0;
+                n = 0;
+                for (i = 0; i < f; i++)
+                {
+                    for (j = 0; j < f; j++)
+                    {
+                        b[i][j] = 0;
+                        if (i != q && j != p)
+                        {
+                            b[m][n] = num[i][j];
+                            if (n < (f - 2))
+                                n++;
+                            else
+                            {
+                                n = 0;
+                                m++;
+                            }
+                        }
+                    }
+                }
+                fac[q][p] = (int) Math.pow(-1, q + p) * calDeterminant(b, f - 1);
+            }
+        }
+        trans(fac, f);
+    }
+
+    void trans(int fac[][], int r)
+    {
+        int i, j;
+        int b[][], inv[][];
+        b = new int[r][r];
+        inv = new int[r][r];
+        int d = calDeterminant(km, r);
+        int mi = mi(d % 26);
+        mi %= 26;
+        if (mi < 0)
+            mi += 26;
+        for (i = 0; i < r; i++)
+        {
+            for (j = 0; j < r; j++)
+            {
+                b[i][j] = fac[j][i];
+            }
+        }
+        for (i = 0; i < r; i++)
+        {
+            for (j = 0; j < r; j++)
+            {
+                inv[i][j] = b[i][j] % 26;
+                if (inv[i][j] < 0)
+                    inv[i][j] += 26;
+                inv[i][j] *= mi;
+                inv[i][j] %= 26;
+            }
+        }
+        //System.out.println("\nInverse key:");
+        //matrixtoinvkey(inv, r);
+
+        invK = inv;
+    }
+
+    public int mi(int d)
+    {
+        int q, r1, r2, r, t1, t2, t;
+        r1 = 26;
+        r2 = d;
+        t1 = 0;
+        t2 = 1;
+        while (r1 != 1 && r2 != 0)
+        {
+            q = r1 / r2;
+            r = r1 % r2;
+            t = t1 - (t2 * q);
+            r1 = r2;
+            r2 = r;
+            t1 = t2;
+            t2 = t;
+        }
+        return (t1 + t2);
+    }
+
+    public void matrixtoinvkey(int inv[][], int n)
+    {
+        String invkey = "";
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                invkey += (char) (inv[i][j] + 97);
+            }
+        }
+        System.out.print(invkey);
+    }
+    public boolean check(String key, int len)
+    {
+        calKeyMatrix(key, len);
+        int d = calDeterminant(km, len);
+        d = d % 26;
+        if (d == 0)
+        {
+            System.out.println("Key is not invertible");
+            return false;
+        }
+        else if (d % 2 == 0 || d % 13 == 0)
+        {
+            System.out.println("Key is not invertible");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    //masih belum bisa :(
+    /*public void keyFinder(String line, String key, int[] lm, int[] lm2){
+        Set<KeyF> foundKeys = new HashSet<KeyF>();
+        calLineMatrix(line);
+        calLineMatrix2(key);
+
+
+
+        // Plaintext
+        int pA = lm[0]; 	// Row 1, Col 1
+        int pB = lm[1];	// Row 1, Col 2
+        int pC = lm[2];	// Row 2, Col 1
+        int pD = lm[3];		// Row 2. Col 2
+
+        // Ciphertext
+        int A = lm2[0];		// Row 1, Col 1
+        int B = lm2[1];		// Row 1, Col 2
+        int C = lm2[2];		// Row 2, Col 1
+        int D = lm2[3];		// Row 2, Col 2
+
+        int keyNum = 1;
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                for (int k = 0; k < 26; k++) {
+                    for (int l = 0; l < 26; l++) {
+                        int kA = i;
+                        int kB = j;
+                        int kC = k;
+                        int kD = l;
+
+                        int cA = (kA * pA) + (kC * pB);
+                        int cB = (kB * pA) + (kD * pB);
+                        int cC = (kA * pC) + (kC * pD);
+                        int cD = (kB * pC) + (kD * pD);
+
+                        while (cA < 0) {
+                            cA += 26;
+                        }
+                        while (cA > 25) {
+                            cA -= 26;
+                        }
+                        while (cB < 0) {
+                            cB += 26;
+                        }
+                        while (cB > 25) {
+                            cB -= 26;
+                        }
+                        while (cC < 0) {
+                            cC += 26;
+                        }
+                        while(cC > 25) {
+                            cC -= 26;
+                        }
+                        while(cD < 0) {
+                            cD += 26;
+                        }
+                        while(cD > 25) {
+                            cD -= 26;
+                        }
+
+//						System.out.println("Key Number: " + keyNum + "/456976" + "   Key: " + kA + " " + kB + " " + kC + " " + kD
+//										 + "   Cipher: " + cA + " " + cB + " " + cC + " " + cD + "	%" + 100 * (double)keyNum / 456976);
+
+                        keyNum++;
+
+                        if (cA == A && cB == B && cC == C && cD == D) {
+//							System.out.println("Key Found!");
+                            KeyF foundKey = new KeyF(kA, kB, kC, kD);
+                            foundKeys.add(foundKey);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!foundKeys.isEmpty()) {
+            System.out.println("Found Keys: ");
+            for (KeyF k : foundKeys) {
+                System.out.println("	Key: " + k.k11 + " " + k.k12 + " " + k.k21 + " " + k.k22);
+            }
+        } else {
+            System.out.println("Keys Not Found!");
+        }
+    }*/
+
+    public static void main(String args[]) throws IOException
+    {
+        Main obj = new Main();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Menu:\n1: Encryption\n2: Decryption\n3: Key Finder");
+        choice = Integer.parseInt(in.readLine());
+
+
+
+        System.out.println("Enter the line: ");
+        String line = in.readLine();
+        System.out.println("Enter the second line: ");
+        String key = in.readLine();
+        double sq = Math.sqrt(key.length());
+
+        /*if (choice == 3){
+            keyFinder(line, key, obj.lm, obj.lm2);
+        }*/
+
+        if (sq != (long) sq)
+            System.out.println("Cannot Form a square matrix");
+        else
+        {
+            int size = (int) sq;
+            if (obj.check(key, size))
+            {
+
+                System.out.println("Result:");
+                obj.cofact(obj.km, size);
+                obj.performDivision(line, size);
+
+
+            }
+        }
+    }
+}
